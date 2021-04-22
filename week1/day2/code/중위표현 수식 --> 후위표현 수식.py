2c37de345d6bfb434947d9f0af8e9a5f6ec0b26d
SeungJun <dzzy6505@gmail.com>
2021-04-22 10:57:56 +0900

    add: update

---
+++ week1/day2/code/중위표현 수식 --> 후위표현 수식.py
@@ -0,0 +1,56 @@
+#%%
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+prec = {
+    '*': 3, '/': 3,
+    '+': 2, '-': 2,
+    '(': 1, ')': 1,
+}
+#%%
+def solution(S):
+    opStack = ArrayStack()
+    answer = []
+        # Iterate over the expression for conversion
+    for char in S:
+        if char.isalpha():
+            answer.append(char)
+
+        elif char == '(':
+            opStack.push(char)
+        elif char == ')':
+            if not opStack.isEmpty():
+                while opStack.peek() != '(':
+                    answer.append(opStack.pop())
+                opStack.pop()
+        else:
+            if not opStack.isEmpty() and prec[opStack.peek()] >= prec[char]:
+                answer.append(opStack.pop())
+                opStack.psuh(char)
+            else:
+                opStack.push(char)
+
+    while not opStack.isEmpty():
+        answer.append(opStack.pop())
+    return "".join(answer)
+# solution('A+B*C')
+solution('(A+(B-C))*D')
+
+# %%
